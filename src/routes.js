2ca7e7436b5445a4f74092d25a95494c46d34553
mdjohn13 <matt.d.johnson@nasa.gov>
2019-10-12 12:28:27 -0700

    add plex endpoint for handling events and posting to slack

--- src/routes.js
+++ src/routes.js
@@ -18,6 +18,22 @@ router.post('/slack/postMessage', async function(req, res, next) {
     await webhook.send(req.body);
     res.send('sent a message!');
 });
+router.post('/plex', async function(req, res, next) {
+
+    const payload = req.body;
+    if (payload.event === 'media.play'){
+        const webhook = new IncomingWebhook(slackConfig.webhookURL);
+        const msg = {
+            text: `Server ${payload.Server.title} is playing the following ${payload.Metadata.type} ${payload.Metadata.title}`
+        };
+
+        await webhook.send(msg);
+        res.send(`sent a message!:${msg}`);
+    } else {
+        res.send(`noop for ${payload.event}`);
+        
+    }
+});
 
 export default router;
   

6efc991b72040c54764792105f33b7f2f144cb4b
mdjohn13 <matt.d.johnson@nasa.gov>
2019-10-12 12:03:00 -0700

    simple endpoint for posting a message to slack

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,22 @@
 import express from 'express'
 import { log } from './util/serverUtils';
+import config from 'config';
+
+const slackConfig = config.get('slack');
+const { IncomingWebhook } = require('@slack/webhook');
+
 const router = new express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource!!!!!');
+});
+
+router.post('/slack/postMessage', async function(req, res, next) {
+    const webhook = new IncomingWebhook(slackConfig.webhookURL);
+    log.info(req);
+    await webhook.send(req.body);
+    res.send('sent a message!');
 });
 
 export default router;
